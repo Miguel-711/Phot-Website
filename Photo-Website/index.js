2a0f9ba4fe3a1580b596b03a6eddd215c6d06b76
Miguel01198 <gonzalez98m@gmail.com>
2025-04-16 17:56:28 -0700

    first commit

---
+++ Photo-Website/index.js
@@ -0,0 +1,6 @@
+let image = ['image1.jpg', 'image2.jpg', 'image3.jpg','image4.jpg'];
+
+setInterval(function () {
+    let random = Math.floor(Math.random() * image.length);
+    document.querySelector('.main-pic').src = `/Photos/${image[random]}`;
+}, 3000);
